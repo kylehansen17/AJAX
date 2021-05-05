47451aceff933fc3690f43568336dc1f969622d3
kylehansen17 <kyle.hansen925@gmail.com>
2021-05-05 07:48:15 -0500

    Fetch and Axios

--- app.js
+++ app.js
@@ -5,3 +5,47 @@ JSON.parse(data)
 
 JSON.stringify(object)
 // turns Javascript object into JSON string
+
+
+// Fetch API - making http requests
+fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(res => {
+        console.log("Response, waiting to parse", res)
+        return res.json()
+    })
+    .then(data => {
+        console.log("Data parsed")
+        console.log(data.ticker.price)
+    })
+    .catch(e => {
+        console.log("Error", e)
+    })
+// cleaned up using async function
+const fetchBitcoinPrice = async () => {
+    try {
+        const res = await fetch('https://api.cryptonator.com/api/ticker/btc-usd')
+        const data = await res.json();
+        console.log(data.ticker.price)
+    } catch (e) {
+        console.log("something went wrong", e)
+    }
+}
+
+
+// Making requests using Axios
+axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+    .then(res => {
+        console.log(res.data.ticker.price)
+    })
+    .catch(err => {
+        console.log("Error", err)
+    })
+// rewriting using async function
+const fetchBitcoinPrice = async () => {
+    try {
+        const res = await axios.get('https://api.cryptonator.com/api/ticker/btc-usd')
+        console.log(res.data.ticker.price)
+    } catch (e) {
+        console.log("Error", e)
+    }
+}
